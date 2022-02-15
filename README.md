f02dcd8c98f5041bfe6d17edcf1f6384fdd86593
jonatan de oliveira <j.jhonis95@gmail.com>
2022-02-14 21:29:13 -0400

    adding README

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Github profile search<h1>
+<h3>description:<h3>
+<p>this project i am using to test javascript promisses<p>
---
+++ script.js
@@ -0,0 +1,6 @@
+const APIURL = 'https://api.github.com/users/'
+
+const form=document.getElementById("form")
+const search=document.getElementById("search")
+const main=document.getElementById("main")
+
